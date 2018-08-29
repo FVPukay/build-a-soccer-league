8c43539bb97bdf3aaac2727efa30511e862701ec
frederickpukay <frederickpukay@gmail.com>
2018-08-28 17:55:56 -0700

    feat: Read the data from the supplied csv file

--- league_builder.py
+++ league_builder.py
@@ -1,3 +1,13 @@
 # Teamtreehouse.com Python Web Development Program
 # Project 1: Build a Soccer League
 # Note: This is designed to work with Python 3
+
+def league_builder():
+    import csv
+
+    player_list = []
+
+    # Read the data from the supplied csv file.
+    with open('soccer_players.csv') as csvfile:
+        player_reader = csv.reader(csvfile, delimiter=',')
+        player_list = list(player_reader)
