b510357fd866555b6ad11f5b7d1f5f5c24a0346b
frederickpukay <frederickpukay@gmail.com>
2018-08-28 17:57:30 -0700

    feat: Store the data in an appropriate data type

--- league_builder.py
+++ league_builder.py
@@ -6,8 +6,23 @@ def league_builder():
     import csv
 
     player_list = []
+    experienced = []
+    inexperienced = []
 
     # Read the data from the supplied csv file.
     with open('soccer_players.csv') as csvfile:
         player_reader = csv.reader(csvfile, delimiter=',')
         player_list = list(player_reader)
+
+    # Store the data in an appropriate data type for the next task.
+    for row in player_list:
+        if row[2] == 'YES':
+            experienced.append(row)
+        elif row[2] == 'NO':
+            inexperienced.append(row)
+    # Remove height
+    else:
+        for row in experienced:
+            del row[1]
+        for row in inexperienced:
+            del row[1]
